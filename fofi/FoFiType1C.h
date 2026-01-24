47e0281ab368032e6d880c1147276e4b05fc8eae
noaione <noaione@n4o.xyz>
2026-01-24 10:38:30 +0700

    FoFiType1C: try adding <array> header

--- fofi/FoFiType1C.h
+++ fofi/FoFiType1C.h
@@ -18,6 +18,7 @@
 // Copyright (C) 2018-2020, 2025, 2026 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2025, 2026 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
+// Copyright (C) 2026 noaione <noaione@n4o.xyz>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
